7ba3468692f18957dfed2335e201cea17cfafe96
DinoPimenteiro <gabrielwilliam234@gmail.com>
2026-04-10 14:23:32 -0300

    update imports

--- src/domain/types/DomainError.ts
+++ src/domain/types/DomainError.ts
@@ -1,6 +1,6 @@
-import type { InvalidEmailError } from "../errors/InvalidEmailError.js";
-import type { InvalidNameError } from "../errors/InvalidNameError.js";
-import type { InvalidPasswordError } from "../errors/InvalidPasswordError.js";
+import type { InvalidEmailError } from "../errors/Exceptions/InvalidEmailError.ts";
+import type { InvalidNameError } from "../errors/Exceptions/InvalidNameError.ts";
+import type { InvalidPasswordError } from "../errors/Exceptions/InvalidPasswordError.ts";
 
 export type DomainError =
   | InvalidEmailError
